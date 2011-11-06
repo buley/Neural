e888d9550ed60b1400d72c13480209743facfe14
Taylor Buley <taylor.buley@gmail.com>
2011-11-06 15:56:59 -0500

    via vim

--- neural.dev.js
+++ neural.dev.js
@@ -796,7 +796,7 @@ var Neural = (function() {
 				Network.get( {  'type': 'synapse', 'on_success': function( returned_synapse_data ) {
 					console.log( 'Public.prototype.add > Network.put success > Network.put error > Network.get success', returned_synapse_data );
 					if( 'undefined' !== typeof on_success ) {
-						on_success( { 'type': 'synapse', 'action': 'get', 'data': new_synapse_data, 'result': returned_synapse_data } );
+						on_success( { 'type': 'synapse', 'action': 'get', 'result': returned_synapse_data } );
 					}
 
 				}, 'on_error': function( context ) {
