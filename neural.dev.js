c2dc2f137d1a19802165c50f1c6da92e96c56fad
Taylor Buley <taylor.buley@gmail.com>
2011-11-13 23:34:24 -0500

    adding a method to build the network

--- neural.dev.js
+++ neural.dev.js
@@ -784,7 +784,7 @@ var Neural = (function() {
 							on_success( hidden_id );
 						}				
 
-						if( expected_actions >= hidden_ids.length ) {
+						if( expected_actions <= hidden_ids.length ) {
 							on_complete( hidden_ids );
 							return this;
 						}
@@ -812,7 +812,7 @@ var Neural = (function() {
 									on_success( hidden_id );
 								}				
 								console.log('HIDDEN COMPAR', expected_actions,hidden_ids.length);
-								if( expected_actions >= hidden_ids.length ) {
+								if( expected_actions <= hidden_ids.length ) {
 									on_complete( hidden_ids );
 									return this;
 								}
@@ -832,7 +832,7 @@ var Neural = (function() {
 								expected_actions -= 1;
 
 								console.log('HIDDEN COMPAR', expected_actions,hidden_ids.length);
-								if( expected_actions >= hidden_ids.length ) {
+								if( expected_actions <= hidden_ids.length ) {
 									on_complete( hidden_ids );
 									return this;
 								}
@@ -850,7 +850,7 @@ var Neural = (function() {
 							}
 
 								console.log('HIDDEN COMPAR', expected_actions,hidden_ids.length);
-							if( expected_actions >= hidden_ids.length ) {
+							if( expected_actions <= hidden_ids.length ) {
 								on_complete( hidden_ids );
 								return this;
 							}
@@ -871,7 +871,7 @@ var Neural = (function() {
 					
 					}
 
-					if( expected_actions >= hidden_ids.length ) {
+					if( expected_actions <= hidden_ids.length ) {
 					
 						on_complete( hidden_ids );
 						return this;
