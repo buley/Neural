6df5d967fb1e4dc24f3e1b71df2f38212472db97
Taylor Buley <taylor.buley@gmail.com>
2011-11-18 21:03:27 -0500

    adding a method to build the network

--- neural.dev.js
+++ neural.dev.js
@@ -1187,8 +1187,7 @@ var Neural = (function() {
 				}, 'on_complete': function( hidden_ids ) {
 					
 					synapses = Public.prototype.zipSynapses( 'hidden', hidden_ids, 'output', output_ids );
-
-					//
+					
 					console.log("ADOPTION AGENCY",synapses);
 
 				}
