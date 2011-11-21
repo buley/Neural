9cdf61568af08deafaddf461d0661ded92a6611d
Taylor Buley <taylor.buley@gmail.com>
2011-11-20 19:58:52 -0500

    adding a method to build the network

--- neural.dev.js
+++ neural.dev.js
@@ -114,8 +114,7 @@ var Neural = (function() {
 
 
 	Public.prototype.incrementer = function( current, type_object ) {
-		//current = ( !isNaN( current ) ) ? current : 0;
-		console.log("JAMIE JIMPLIN OR JANET JOPLER",current,current+1);
+		current = ( !isNaN( current ) ) ? current : 0;
 		return current + 1;
 	};
 
