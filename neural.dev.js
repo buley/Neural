c1834bcd5e05a5fb6c073d5e1c2ee2f0cd29f2aa
Taylor Buley <taylor.buley@gmail.com>
2011-11-21 22:28:00 -0500

    adding a method to build the network

--- neural.dev.js
+++ neural.dev.js
@@ -1289,6 +1289,8 @@ var Neural = (function() {
 
 		expected_actions = Public.prototype.countAttributes( additions );
 
+		additions = additions.sort( function(a,b){return (a<b) ? -1 : 1; } ) );
+				}
 		for( x in additions ) {
 			if( additions.hasOwnProperty( x ) ) {
 		
