5cc68be4fe8b8e9a84472226c2044d862530f9e7
Taylor Buley <taylor.buley@gmail.com>
2011-11-22 19:31:34 -0500

    adding a method to build the network

--- neural.dev.js
+++ neural.dev.js
@@ -3154,7 +3154,7 @@ console.log("AWSOME",JSON.stringify(new_synapse_data));
 						var neuron = Public.prototype.returnNeuron( attr );
 						results[ neuron.display ] = { 
 							'neuron': neuron
-							, 'score': tanh( matrix[ attr ] )
+							, 'score': matrix[ attr ]
 						};
 					}
 				}
